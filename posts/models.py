ea63f75e2892663dded00a48ca7251f337888ae7
suhy <hisuhy065@gmail.com>
2024-08-12 13:52:07 +0900

    좋아요 기능 관련 모델링 M to M

--- posts/models.py
+++ posts/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     )
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
 
 
 class Comment(models.Model):
